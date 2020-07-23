116a63a1f0d8437d4d8cdec27f060efd9c049545
jobayer-h <jobayer.dev@gmail.com>
2020-07-23 23:18:34 +0600

    using function

--- script.js
+++ script.js
@@ -1,99 +1,62 @@
 // unit plus 
 const plusBtn = document.getElementById('plus');
 plusBtn.addEventListener('click', function(){
-    const unit = document.getElementById('unit').value;
-    const getUnit= parseFloat(unit);
-
-    const updateUnit = getUnit + 1;
-    document.getElementById('unit').value = updateUnit;
-
-    const price = document.getElementById('price').innerText;
-    const getPrice = parseFloat(price);
-
-    const updatePrice  = 1219 * updateUnit;
-    document.getElementById('price').innerText = updatePrice;
-
-    const total = document.getElementById('subTotal').innerText;
-    const totalPrice = parseFloat(total);
-
-    const updateTotalPrice = totalPrice + 1219 ;
-    document.getElementById('subTotal').innerText = updateTotalPrice;
-
-    document.getElementById('total').innerText = updateTotalPrice;
+    var totalUnit = addUnit('unit');
+    updatePrice('price',1219, totalUnit);
+    updateTotalPrice('subTotal',1219);
 })
+
 const plusBtn2 = document.getElementById('plus2');
 plusBtn2.addEventListener('click', function(){
-    const unit = document.getElementById('unit2').value;
-    const getUnit= parseFloat(unit);
-
-    const updateUnit = getUnit + 1;
-    document.getElementById('unit2').value = updateUnit;
-
-    const price = document.getElementById('price2').innerText;
-    const getPrice = parseFloat(price);
-
-    const updatePrice  = 59 * updateUnit;
-    document.getElementById('price2').innerText = updatePrice;
-
-    const total = document.getElementById('subTotal').innerText;
-    const totalPrice = parseFloat(total);
-
-    const updateTotalPrice = totalPrice + 59 ;
-    document.getElementById('subTotal').innerText = updateTotalPrice;
-    
-    document.getElementById('total').innerText = updateTotalPrice;
+    var totalUnit = addUnit('unit2');
+    updatePrice('price2',59, totalUnit);
+    updateTotalPrice('subTotal',59);
 })
 
 //unit minus
-
 const minusBtn = document.getElementById('minus');
 minusBtn.addEventListener('click', function(){
-
-    const unit = document.getElementById('unit').value;
-    const getUnit= parseFloat(unit);
-
-    const updateUnit = getUnit - 1;
-    document.getElementById('unit').value = updateUnit;
-    
-
-    const price = document.getElementById('price').innerText;
-    const getPrice = parseFloat(price);
-
-    const updatePrice  = 1219 * updateUnit;
-    document.getElementById('price').innerText = updatePrice;
-
-    const total = document.getElementById('subTotal').innerText;
-    const totalPrice = parseFloat(total);
-
-    const updateTotalPrice = totalPrice - 1219 ;
-    document.getElementById('subTotal').innerText = updateTotalPrice;
-
-    document.getElementById('total').innerText = updateTotalPrice;
-    
+    var totalUnit = removeUnit('unit');
+    updatePrice('price',1219, totalUnit);
+    updateTotalPrice('subTotal',-1219);
 })
 
 const minusBtn2 = document.getElementById('minus2');
 minusBtn2.addEventListener('click', function(){
-
-    const unit = document.getElementById('unit2').value;
-    const getUnit= parseFloat(unit);
-
-    const updateUnit = getUnit - 1;
-    document.getElementById('unit2').value = updateUnit;
-    
-
-    const price = document.getElementById('price2').innerText;
-    const getPrice = parseFloat(price);
-
-    const updatePrice  = 59 * updateUnit;
-    document.getElementById('price2').innerText = updatePrice;
-
-    const total = document.getElementById('subTotal').innerText;
-    const totalPrice = parseFloat(total);
-
-    const updateTotalPrice = totalPrice - 59 ;
-    document.getElementById('subTotal').innerText = updateTotalPrice;
-
-    document.getElementById('total').innerText = updateTotalPrice;
-    
+    var totalUnit = removeUnit('unit2');
+    updatePrice('price2',59, totalUnit);
+    updateTotalPrice('subTotal',-59);
 })
+
+// functions
+function addUnit(id) {
+    const unit = document.getElementById(id).value;
+    const getUnit= parseFloat(unit);
+    const updateUnit = getUnit + 1;
+    document.getElementById(id).value = updateUnit;
+    return updateUnit;
+}
+
+function removeUnit(id) {
+    const unit = document.getElementById(id).value;
+    const getUnit= parseFloat(unit);
+    const updateUnit = getUnit - 1;
+    document.getElementById(id).value = updateUnit;
+    return updateUnit;
+}
+
+function updatePrice(id, single, totalUnit) {
+    const price = document.getElementById(id).innerText;
+    const getPrice = parseFloat(price);
+    const updatePrice  = single * totalUnit;
+    document.getElementById(id).innerText = updatePrice;
+    return updatePrice;
+}
+
+function updateTotalPrice(id,single) {
+    const total = document.getElementById(id).innerText;
+    const totalPrice = parseFloat(total);
+    const updateTotalPrice = totalPrice + single ;
+    document.getElementById(id).innerText = updateTotalPrice;
+    document.getElementById('total').innerText = updateTotalPrice;
+}
